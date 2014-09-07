13af7a289f03343cbc745175f436a688e8588e19
Zheng Jia Ning <jianingzheng@gmail.com>
2014-09-07 20:09:31 +0800

    validate email format.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   validates_presence_of :password
 
   validates_uniqueness_of :email
-
+  validates_format_of  :email, :message => "email format not correct!", :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 end
